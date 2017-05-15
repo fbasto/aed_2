c9b5879ebbeb2e04f0709c3051bc106fdfffc372
Rui Pedro Ferreira Mendes <ruimendes_10@hotmail.com>
2017-05-15 18:05:20 +0100

    gerador agora dá as Edges para serem adicionadas no mapsolver

--- gerador.py
+++ gerador.py
@@ -13,16 +13,16 @@ def mapas(num):
                 if(matrix[i][k] == 0):
                     dist = random.randint(1,number*100)
                 while(check(dist) == True):
-                    print (dist)
+                    # print (dist)
                     dist = random.randint(1,number*100)
                 values.append(dist)
                 matrix[i][k] = dist
                 matrix[k][i] = dist
-
+    # print("M:",matrix)
     create_file(number,matrix)
 
 def check(n):
-    print("CHECK: ",n)
+    # print("CHECK: ",n)
     if(n in values):
         return True
     else:
@@ -33,17 +33,22 @@ def check(n):
 def create_file(number,matrix):
     filename = "tarefa_" + str(number) + ".txt"
     f = open(filename,'w')
+    f.write("startin 1\n")
     for i in range(number):
         aux = ""
         for j in range(number):
-            if (j == number-1):
-                aux = aux + str(matrix[i][j]) + "\n"    
-            else:  
-                aux = aux + str(matrix[i][j]) + " "    
-
-            
-        f.write(aux)
-        print("String: ",aux)
+            # if (j == number-1):
+            #     aux = aux + str(matrix[i][j]) + "\n"    
+            # else:  
+            #     aux = aux + str(matrix[i][j]) + " "
+            if(i<j):
+                # print("i=",i,"; j=",j,"; peso=",matrix[i][j])
+                aux = str(i+1) + " " + str(j+1) + " " + str(matrix[i][j]) + "\n"
+                # else:
+                #     aux = str(i) + str(j) + str(matrix[i][j]) + "\n"
+                # print("String: ",aux)
+                f.write(aux)
+    f.write("0")
     f.close()
 
 
