9e7339a1b6b9963fd17020251c6ea1c0aa3f5c8c
Rui Pedro Ferreira Mendes <ruimendes_10@hotmail.com>
2017-05-18 17:17:36 +0100

    ready to test tarefa1

--- MapSolver.py
+++ MapSolver.py
@@ -1,4 +1,5 @@
 from tsp_solver import *
+import time
 
 class Graph:
 	def __init__(self):
@@ -77,6 +78,7 @@ def calcDistance(path, matrix):
 	for i in range(1,len(path)):
 		# print("traveling from",priorvertex+1,"to",i+1)
 		dist = dist + int(matrix[int(path[priorvertex])-1][int(path[i])-1])
+		# print("dist=",dist,"+",int(matrix[int(path[priorvertex])-1][int(path[i])-1]),"from path[",priorvertex,"][",i,"]")
 		priorvertex = i
 		# print("dist updated to:",dist)
 	dist = dist + int(matrix[int(path[priorvertex])-1][int(path[0])-1])
@@ -101,56 +103,61 @@ def dfs_paths(graph, start):
 def dfs_paths_optimized(graph,start):
 	print("TODO")
 
+if __name__ == "__main__":
+	g = Graph()
+	print("Para adicionar o nó inicial: \"startin <nome>\" ;\nPara adicionar ligação entre dois nós: \"<nó1> <nó2> <peso>\" .")
+	while(True):
+		userinput = input().split(' ')
+		if(userinput[0]=='M'):
+			#print("breaking because EOF")
+			break
+		if(userinput[0]=='startin'):
+			#print("startin")
+			g.addVertex(userinput[1])
+			g.getVertex(userinput[1]).starter()
+		else:
+			node1 = userinput[0]
+			node2 = userinput[1]
+			weight = userinput[2]
+			if(g.getVertex(node1) == None):
+				g.addVertex(node1)
+			if(g.getVertex(node2) == None):
+				g.addVertex(node2)
+			g.addEdge(node1,node2,weight)
+	# print(g.getVertex('1').getWeight(g.getVertex('2')))
+	matrix = [[0 for x in range(len(g.getVertices()))] for y in range(len(g.getVertices()))] 
+	i=0
+	while(True):
+		userinput = input().split(' ')
+		if(userinput[0]=='E'):
+			#print("breaking because EOF")
+			break
+		else:
+			for j in range(0,len(g.getVertices())):
+				matrix[i][j] = userinput[j]
+		i=i+1
+	printMatrix(matrix)
 
-g = Graph()
-print("Para adicionar o nó inicial: \"startin <nome>\" ;\nPara adicionar ligação entre dois nós: \"<nó1> <nó2> <peso>\" .")
-while(True):
-	userinput = input().split(' ')
-	if(userinput[0]=='M'):
-		#print("breaking because EOF")
-		break
-	if(userinput[0]=='startin'):
-		#print("startin")
-		g.addVertex(userinput[1])
-		g.getVertex(userinput[1]).starter()
-	else:
-		node1 = userinput[0]
-		node2 = userinput[1]
-		weight = userinput[2]
-		if(g.getVertex(node1) == None):
-			g.addVertex(node1)
-		if(g.getVertex(node2) == None):
-			g.addVertex(node2)
-		g.addEdge(node1,node2,weight)
-# print(g.getVertex('1').getWeight(g.getVertex('2')))
-matrix = [[0 for x in range(len(g.getVertices()))] for y in range(len(g.getVertices()))] 
-i=0
-while(True):
-	userinput = input().split(' ')
-	if(userinput[0]=='E'):
-		#print("breaking because EOF")
-		break
-	else:
-		for j in range(0,len(g.getVertices())):
-			matrix[i][j] = userinput[j]
-	i=i+1
-printMatrix(matrix)
+
+	# debug
+	# for i in g.getVertices():
+	# 	print(g.getVertex(i))
 
 
-# debug
-# for i in g.getVertices():
-# 	print(g.getVertex(i))
+	start=time.time()
 
-paths = list(dfs_paths(g,g.getStarter().getId()))
-bestPath = 0
-bestDist = 0
-for i in list(paths):
-	print(i)
-	newDist = calcDistance(i,matrix)
-	if bestDist == 0 or (bestPath[0] != 0 and newDist < bestDist):
-		# print("bestDist=",bestDist," e bestPath=",bestPath)
-		bestDist = newDist
-		bestPath = i
+	paths = list(dfs_paths(g,g.getStarter().getId()))
+	bestPath = 0
+	bestDist = 0
+	for i in list(paths):
+		# print(i)
+		newDist = calcDistance(i,matrix)
+		if bestDist == 0 or (bestPath[0] != 0 and newDist < bestDist):
+			# print("bestDist=",bestDist," e bestPath=",bestPath)
+			bestDist = newDist
+			bestPath = i
 
-print("Encontrou o melhor caminho:",bestPath,"com distância de",bestDist)
+	print("Encontrou o melhor caminho:",bestPath,"com distância de",bestDist)
 
+	end = time.time()
+	print("Operacao demorou: %.10f segundos"%(end-start))
